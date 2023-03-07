0647eeb10cfb8d7398af8bf9ba39c1c7312cdf45
User <egemberdievasafia@gmail.com>
2023-03-07 20:15:23 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+              int [] arr = new int[10];
+            arr [0] =7;
+          arr [1]=9;
+        arr[2]=5;
+              arr[3]=5;
+            arr[4]=5;
+          arr[5]=5;
+            arr[6]=5;
+            arr[7]=5;
+           arr[8]=5;
+            arr[9]=5;
+             System.out.println(arr[9]);
+        int [] tyt= {10,5,10,1,7,8,};
+              int sum = 0;
+            for (int num: arr){
+              sum = sum + num;
+         System.out.println( sum / arr.length);
+        int [] numbers = new int[5];
+        numbers[0] = 10;
+        System.out.println(0);
+        System.out.println(1);
+        String[] strings = new String[3];
+        strings[0] = "привет";
+        strings[1] = "пока";
+        strings[2] = "джава";
+        System.out.println(strings[0]); // это напишеть привет джава
+        System.out.println(strings[2]); //а это привет пока джава
+
+        for (int i = 0; i<strings.length; i++){
+            System.out.println(strings[i]);
+            int [] dffg = {12,34,56,};
+            System.out.println(arr[2]);
+        //    012 крч твоя дз это делать как хоешь и по разному
+        Random random = new Random();
+        int[] arr1 = new int[10];
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+        int num4 = 0;
+        for (int i1=0; i < arr1.length; i++);
+        System.out.println();
+
+        }
+    }
+}
+}
